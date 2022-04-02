ac1c974421b6c6aad0a4bb69e1f6e813280cabc6
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-02 12:09:58 +0100

    Heyy

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,21 +1,8 @@
-// const list = document.querySelector('.output ul');
-// list.innerHTML = '';
-// const greetings = ['Happy Birthday!',
-//     'Merry Christmas my love',
-//     'A happy Christmas to all the family',
-//     'You\'re all I want for Christmas',
-//     'Get well soon'];
 
-// for (let greeting of greetings) {
-//     // Your conditional test needs to go inside the parentheses
-//     // in the line below, replacing what's currently there
-//     if (greeting) {
-//         const listItem = document.createElement('li');
-//         listItem.textContent = greeting;
-//         list.appendChild(listItem);
-//     }
-// }
-
+console.log('Hello World');
+let RanNum = Math.floor(Math.random() * 10);
+console.log(RamNum);
 if (1 + 1 === 2) {
     console.log("Math still works!!!")
 }
+console.log('After World');
