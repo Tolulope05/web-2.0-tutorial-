e539cf98de0d8cf7a5aa44b35e8a2b1450822105
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-02 12:34:37 +0100

    if and else

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,26 @@
-
-console.log('Hello World');
+// console.log('Hello World');
+/**
+ * Random Number Generator
+ * define a function to generate random number
+ * @param {number} Random
+ * if the random number is less than 5, return true
+ * return Number greater than 5!!!
+ * return random number
+ * else
+ * return false
+ * rturn number is less than 5 !!!
+ * return random number
+ * @log {number}
+ * 
+ */
 let RanNum = Math.floor(Math.random() * 10);
-console.log(RamNum);
-if (1 + 1 === 2) {
-    console.log("Math still works!!!")
-}
-console.log('After World');
+console.log(RanNum);
+if (RanNum > 5) {
+    console.log("Random greater than 5!!!");
+    console.log(RanNum);
+} else {
+    console.log("Number is less than 5!!!");
+    if 
+    console.log(RanNum);
+} // end of if else statement
+
