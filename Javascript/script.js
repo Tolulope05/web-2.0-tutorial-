06de91620fdaedfaf29d54812a5d29674727ca6f
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-02 12:01:40 +0100

    fixed

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,17 +1,21 @@
-const list = document.querySelector('.output ul');
-list.innerHTML = '';
-const greetings = ['Happy Birthday!',
-    'Merry Christmas my love',
-    'A happy Christmas to all the family',
-    'You\'re all I want for Christmas',
-    'Get well soon'];
+// const list = document.querySelector('.output ul');
+// list.innerHTML = '';
+// const greetings = ['Happy Birthday!',
+//     'Merry Christmas my love',
+//     'A happy Christmas to all the family',
+//     'You\'re all I want for Christmas',
+//     'Get well soon'];
 
-for (let greeting of greetings) {
-    // Your conditional test needs to go inside the parentheses
-    // in the line below, replacing what's currently there
-    if (greeting) {
-        const listItem = document.createElement('li');
-        listItem.textContent = greeting;
-        list.appendChild(listItem);
-    }
+// for (let greeting of greetings) {
+//     // Your conditional test needs to go inside the parentheses
+//     // in the line below, replacing what's currently there
+//     if (greeting) {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = greeting;
+//         list.appendChild(listItem);
+//     }
+// }
+
+if (1 + 1 === 2) {
+    console.log("Math still works!!!")
 }
