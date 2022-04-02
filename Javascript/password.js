817ad459a83e4c61433dbab7f6234ad05c9d661f
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-02 14:12:03 +0100

    value of number

--- Javascript/password.js
+++ Javascript/password.js
@@ -1,6 +1,32 @@
 /**
  * define a function to generate password
- * password must not be more than 6+ characters
+ * password must not be greater than 6+ characters
  * password must not include space
  */
 
+// Ways around the concepts
+
+// const password = prompt('Please enter a password less than 6 digit');
+// if (password.length >= 6) {
+//     console.log('Password is successful')
+// }
+// else {
+//     console.log('password is longer than 6 digits')
+// }
+// if (password.indexOf(' ') === -1) {
+//     console.log('You dont have a space. GJ!')
+// } else {
+//     console.log('You have space in your password');
+// } // 2 logics that deifnes what we want
+
+const password = prompt('Please enter a password less than 6 digit');
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log('Valid Pasword')
+    } else {
+        console.log('Password cannot contain spaces');
+    }
+}
+else {
+    console.log('password cannot be less than 6 characters')
+} // nested logics 
