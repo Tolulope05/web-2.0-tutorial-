dc17bbb7da0d78462e9235946fb6116a61f88e96
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-03 22:28:07 +0100

    object literal

--- Javascript/jsObject.js
+++ Javascript/jsObject.js
@@ -2,10 +2,16 @@
 // Object is a collection of proerties
 // properties uses a key value parseInt
 
-const fitBitData = {
+const fitData = {
     totalSteps: 3087,
     totalMiles: 211.7,
     avgCalorieBurn: 5755,
     workoutsThisWeek: '5 of 7',
     avgGoodSleep: '3:14'
 };
+
+// Key vlaue pair
+const person = {
+    firstName: 'Tolulope',
+    lastName: 'Fakunle'
+}
