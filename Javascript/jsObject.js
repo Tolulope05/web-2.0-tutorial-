74965627b3d4d16f59a54f1c09b788336f8cf8a3
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-03 22:26:33 +0100

    end of array

---
+++ Javascript/jsObject.js
@@ -0,0 +1,11 @@
+// Using an Object 
+// Object is a collection of proerties
+// properties uses a key value parseInt
+
+const fitBitData = {
+    totalSteps: 3087,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '3:14'
+};
