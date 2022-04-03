463cbc00ef898f83d8973c329438affb65918e84
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-03 05:05:48 +0100

    Tolulope

--- Javascript/logical-operator.js
+++ Javascript/logical-operator.js
@@ -5,12 +5,12 @@
  * ! - Not: IT IS AN INVERSE RLATIONSHIP
  */
 
-const password = prompt('Enter a Vaid Password');
-if (password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log('Valid Password!');
-} else {
-    console.log('Try again..Invalid Password!!')
-} //both sides need to be true
+// const password = prompt('Enter a Vaid Password');
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log('Valid Password!');
+// } else {
+//     console.log('Try again..Invalid Password!!')
+// } //both sides need to be true
 
 
 /**
@@ -21,13 +21,25 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
  * age 65+ SENIOR $10
  */
 
-const age = 65;
-if (age <= 5) {
-    console.log('FREE')
-} else if (age <= 10) {
-    console.log('$10')
-} else if (age <= 65) {
-    console.log('$20')
-} else if (age >= 65) {
-    console.log('$10')
-}
+// const age = -10;
+// if ((age >= 0 && age < 5) || age >= 65) {
+//     console.log('FREE')
+// } else if (age >= 5 && age < 10) {
+//     console.log('$10')
+// } else if (age > 10 && age < 65) {
+//     console.log('$20')
+// } else {
+//     console.log('Invalid age!!')
+// } // End of OR logical operator
+
+/**
+ * Not logical expression
+ * We use !
+ */
+
+const firstName = prompt('Enter First Name');
+if (!firstName) {
+    console.log('Invalid Name...Enter correct First Name')
+} else {
+    console.log('Frist Name entered correctly')
+} //Its goig to return true if the expression we are applying it to returns false
