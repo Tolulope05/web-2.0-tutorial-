395e8e67b25fa683c163957c4d4f9d035efd137c
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-04 05:39:45 +0100

    tips

--- Javascript/jsLoops.js
+++ Javascript/jsLoops.js
@@ -11,6 +11,17 @@
  * )
  */
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i); //Anything you put in here will be dispLAYED 10 TIMES
-};
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i); //Anything you put in here will be dispLAYED 10 TIMES
+// };
+
+
+// Print out even numbers from 0 to 20
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+// Print out 100 to 0
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i)
+} //We can use  *=, /= too
