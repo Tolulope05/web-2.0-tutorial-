b6ea12d621b7c2cf645721b3a47cff937cf468e5
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-04 05:33:27 +0100

    loop

---
+++ Javascript/jsLoops.js
@@ -0,0 +1,16 @@
+/**
+* Loops allow us to repeat codes
+* Types: for, while, for...in, for...of, do...while
+*/
+
+//Print a number from 1 to 10
+/** for(
+ *       [Initial expression]; 
+ *       [condition];
+ *       [incrementalExpression]
+ * )
+ */
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i); //Anything you put in here will be dispLAYED 10 TIMES
+};
