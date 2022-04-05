b757b381c03f020b2c2b094199a7a51c9cc07d6d
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-05 20:37:29 +0100

    Javascript

--- Javascript/jsFunctions.js
+++ Javascript/jsFunctions.js
@@ -1,9 +1,12 @@
 /**
- * Functions
+  * =================================
+ * Function
+ * =================================
  * - Functions allows a reusable moular code
  * - we define a chunk of code that we can use later
  * - We can use it anytime
  * - We can pass in arguments into a
+ * MB: We pass a *parameter* into a function e.g firstName
  */
 
 // function rollDie() {
@@ -12,8 +15,53 @@
 // } //rollDie()
 
 
-function greet(firstName) {
-    console.log(`Hi ${firstName}. Its so great to finally meet you.`)
+// function greet(firstName) {
+//     console.log(`Hi ${firstName}. Its so great to finally meet you.`)
+// }
+// greet('tolu') //Hi tolu. Its so great to finally meet you./
+// greet('nonso') //Hi nonso. Its so great to finally meet you./
+
+
+/**
+ * =================================
+ * Function
+ * =================================
+ * A function that sum two numbers
+ * A function that subtracts two numbers
+ * A function that multiplies two numbers
+ * A function that divides two numbers
+ * A function that raise two numbers to power of 
+ */
+
+function sum(a, b) {
+    console.log(a + b)
+} //sum(5,6) //11
+
+function subtract(a, b) {
+    console.log(a - b)
+} //sub(6,6) //1
+
+function multiply(a, b) {
+    console.log(a * b)
+} //multiply
+
+function divide(a, b) {
+    console.log(a / b)
 }
-greet('tolu') //Hi tolu. Its so great to finally meet you./
-greet('nonso') //Hi nonso. Its so great to finally meet you./
+
+function power(a, b) {
+    console.log(a ** b)
+}
+
+#include _MAIN_H_
+#include stdio.h 
+
+int a;
+int b;
+
+function power(a, b) {
+    printf(a + b);
+    if 
+}
+
+
