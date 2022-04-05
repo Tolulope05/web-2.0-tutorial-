afe531646dbc734b7f49565b76b5c37d14fa7df4
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-05 14:03:39 +0100

    mt todo success

---
+++ Project_todo-list/README.md
@@ -0,0 +1,6 @@
+### A TODO LIST
+
+>>Press 'new' - Add a Todo
+>>Press 'list' - List all todo
+>> Press 'delete' - remove specific Todo using splice method
+>>Press 'quit' - quit App
--- Project_todo-list/scipt.js
+++ Project_todo-list/scipt.js
@@ -1,1 +1,29 @@
-console.log('Hello This is Todo list intro')
+/**
+ * Todo List
+ * 'new' - Add a Todo
+ * 'list' - List all todo
+ * 'delete' - remove specific Todo using splice method
+ * 'quit' - quit App 
+ */
+let todo = prompt('Hi!, Please add a new task');
+let storage = [];
+
+while (todo) {
+    if (todo === 'quit' || todo === 'q') {
+        alert('See You later chief');
+        break;
+    }
+    storage.push(todo);
+    todo = prompt(`You added ${todo} to your task \n Please enter new task`);
+    if (todo === 'list') {
+        console.log(storage);
+        prompt(`Your task are: ${storage}`);
+        todo = prompt('enter new task');
+    }
+    if (todo === 'delete') {
+        storage.pop();
+        todo = prompt('enter new task');
+    }
+
+}
+
