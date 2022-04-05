865f1ab2a3b7e55dd14ca5fee9a704d83a213710
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-05 20:17:51 +0100

    function parameters

---
+++ Javascript/jsFunctions.js
@@ -0,0 +1,14 @@
+/**
+ * Functions
+ * - Functions allows a reusable moular code
+ * - we define a chunk of code that we can use later
+ * - We can use it anytime
+ * - We can pass in arguments into a
+ */
+
+// function rollDie() {
+//     let rand = Math.floor(Math.random() * 10) + 1;
+//     console.log(rand)
+// } //rollDie()
+
+
