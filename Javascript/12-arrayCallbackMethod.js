ff649d0f438da72d01ff47028f397f2e3c2b776a
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-09 16:46:31 +0100

    For  each

--- Javascript/12-arrayCallbackMethod.js
+++ Javascript/12-arrayCallbackMethod.js
@@ -1,7 +1,7 @@
 /**
  * Using the new arrow functions syntax
  * understanding the following methods:
- * FroEach
+ * ForEach
  * map
  * filter
  * find
@@ -10,9 +10,48 @@
  * every
  */
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
 const result = words.filter(word => word.length > 6);
 
 console.log(result);
- // expected output: Array ["exuberant", "destruction", "present"]
+// expected output: Array ["exuberant", "destruction", "present"]
+
+
+/**
+ * Find all prime numbers in an array
+ */
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+    for (let i = 2; num > i; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
