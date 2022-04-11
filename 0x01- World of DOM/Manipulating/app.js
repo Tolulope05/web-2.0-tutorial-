76f34c02738029be7dd4cbbedc01521d7c89bc29
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-11 18:48:56 +0100

    Selecting method

--- 0x01- World of DOM/Manipulating/app.js
+++ 0x01- World of DOM/Manipulating/app.js
@@ -1,3 +1,31 @@
+/**
+ * ELEMENT
+ * =======
+ * Event target <- Node <- Element
+ * - Element inherits properties from its parent interface, Node,
+ *   and by extension that interface's parent, EventTarget.
+ *  https://developer.mozilla.org/en-US/docs/Web/API/Element
+ * 
+ * SELECTING Methods
+ * =================
+ * getElementById() => get any element by its id
+ * GetElementByClassName() => get any element by its class name
+ * GetElementByTagName() => get any element by its tag name
+ * querySelector() => can get any element by its id (#) or class (.) or tagname or attribute
+ */
+
+// const allImages = document.getElementsByTagName('img');
+// for (let img of allImages) {
+//     console.log(img.src);
+//     img.src = 'any img dir';
+// }
+
+
+const squareImg = document.getElementsByClassName('square');
+for (let img of squareImg) {
+    console.log(img.src)
+}
+
 const allLinks = document.querySelectorAll('a');
 
 // for (let link of allLinks) {
@@ -11,8 +39,4 @@ for (let link of allLinks) {
     link.style.textDecorationStyle = 'wavy'
 }
 
-// const allImages = document.getElementsByTagName('img');
-// for (let img of allImages) {
-//     console.log(img.src);
-//     img.src = 'any img dir';
-// }
+const anchorJava = document.querySelector("a[title = 'Java']") //Get element by attribute
