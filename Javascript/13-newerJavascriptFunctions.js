b7224d55a879c180fbe6deb4827b1639c8a35577
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-11 06:15:35 +0100

    Default parameters

---
+++ Javascript/13-newerJavascriptFunctions.js
@@ -0,0 +1,6 @@
+/**
+ * Default parameters
+ * e.g I want user to roll a die and i want default numbers if they dont pass any number in
+ */
+
+const rollDie = (num) => (Math.floor(Math.random() * num) + 1);
