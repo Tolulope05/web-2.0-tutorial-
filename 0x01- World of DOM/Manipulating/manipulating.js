8fcbab36fbb5dfcdd1b1fc28cd6b6ca61d9bd9b7
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-11 22:26:00 +0100

    GEt attribute

--- 0x01- World of DOM/Manipulating/manipulating.js
+++ 0x01- World of DOM/Manipulating/manipulating.js
@@ -23,6 +23,7 @@
  */
 
 // Always use console.dir() to check the dom property of any element
+// innerHTML retrives everything inside the text including the tag names
 // console.log(document.querySelector('p').innerText)
 // document.querySelector('p').innerText = 'lolololololololol' 
 
@@ -32,8 +33,29 @@ const allLinks = document.querySelectorAll('a');
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
+// for (let link of allLinks) {
+//     link.style.color = 'rgb(0, 108, 134)';
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+// }
+
+/**
+ * ATTRIBUTES
+ * ===========
+ * All attributes can be accessed directly
+ */
+
+// document.querySelector('img').id = 'newid'; // Changes the id of the img tag
+// document.querySelector('img').src = 'newsrc'; // Changes the src of the img tag
+
+// Using Get Attribute
+const firstLink = document.querySelector('a');
+firstLink.href //'http://127.0.0.1:5500/wiki/List_of_chicken_breeds'
+firstLink.title //'List of chicken breeds'
+
+firstLink.getAttribute('href'); //'/wiki/List_of_chicken_breeds'
+firstLink.getAttribute('title'); // 'List of chicken breeds'
+firstLink.getAttribute('id'); // null
+
+// Using Set Attribute
+firstLink.setAttribute('href', 'https://www.google.com');
