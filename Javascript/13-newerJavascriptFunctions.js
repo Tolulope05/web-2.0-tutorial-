ab8b18d23a66d865deca4d3df5d645984c305e20
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-11 06:53:01 +0100

    New parameter pass in

--- Javascript/13-newerJavascriptFunctions.js
+++ Javascript/13-newerJavascriptFunctions.js
@@ -1,6 +1,48 @@
 /**
  * Default parameters
- * e.g I want user to roll a die and i want default numbers if they dont pass any number in
+ * e.g I want user to roll a die and I want default numbers if they dont pass any number in
  */
 
-const rollDie = (num) => (Math.floor(Math.random() * num) + 1);
+// const rollDie = function (num) {
+//     num = typeof num !== 'undefined' ? num : 6; // rollDie() still gives out a value 
+//     num = typeof num >= 6 ? num : 6; //rollDie(30) will still give a random number between 1-6
+//     return Math.floor(Math.random() * num) + 1
+// }; // Old way of doing things.
+
+// const rollDie = function (num) {
+//     if (num === undefined) {
+//         num = 6
+//     }; // rollDie() still gives out a value
+
+//     if (num >= 6) {
+//         num = 6
+//     }; //rollDie(30) will still give a random number between 1-6
+
+//     return Math.floor(Math.random() * num) + 1;
+// };  // Old Way of doing it.
+
+/**
+ * NEW way of passing Default parameter
+ * 
+ */
+const rollDie = function (num = 6) {
+
+    return Math.floor(Math.random() * num) + 1;
+};
+
+// const greet = function (msg, person) {
+//     return `${msg}, ${person}!`
+// }
+
+// const greet = function (msg = 'Hey There', person) {
+//     return `${msg}, ${person}!`
+// } //your default paramter should come second after all the parameter
+// greet("Tolu") // 'Tolu, undefined!'
+
+const greet = function (person, msg = 'Hey There') {
+    return `${msg}, ${person}!`
+} // greet("Tolu") //'Hey There, Tolu!'
+  // greet('Whatsup','Tolulope') //'Tolulope, Whatsup!'
+
+
+
