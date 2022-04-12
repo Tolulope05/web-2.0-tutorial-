b2b226608ee6f8394f46126015426160eac51205
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 00:30:31 +0100

    Dom method

--- 0x01- World of DOM/0xx1-1-DoM event/script.js
+++ 0x01- World of DOM/0xx1-1-DoM event/script.js
@@ -1,1 +1,21 @@
 const btn = document.querySelector('#btn');
+const btn2 = document.querySelector('#btn2');
+
+btn.onclick = function () {
+    console.log('button 2 Clicked');
+    console.log('Yes!!!!!!!!!!');
+}
+const mousEnter = () => { console.log('Ouch Thats a tip') };
+const mousLeave = () => { console.log('Dont leave me!!') };
+
+btn2.onmouseenter = mousEnter
+btn2.onmouseleave = mousLeave
+
+/**
+ * ADD Event Listener
+ * ==================
+ * Specify the evemt type and a callback to run
+ */
+const h2 = document.querySelector('h2');
+h2.addEventListener('click', () => alert('Users clicked on Header 2'));
+
