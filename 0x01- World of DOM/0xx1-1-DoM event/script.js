429be0113e809bbda6e0cc3340f1a927b0001c90
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 00:55:10 +0100

    addeventlistener

--- 0x01- World of DOM/0xx1-1-DoM event/script.js
+++ 0x01- World of DOM/0xx1-1-DoM event/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('#btn');
 const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
 
 btn.onclick = function () {
     console.log('button 2 Clicked');
@@ -19,3 +20,12 @@ btn2.onmouseleave = mousLeave
 const h2 = document.querySelector('h2');
 h2.addEventListener('click', () => alert('Users clicked on Header 2'));
 
+function twist() {
+    console.log('twist');
+}
+function shout() {
+    console.log('shout');
+}
+
+btn3.addEventListener('click', twist)// to have as many call backs as we want
+btn3.addEventListener('click', shout)// to have as many call backs as we want
