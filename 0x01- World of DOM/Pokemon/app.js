692a7c30943fc775dc19bd2653424a564ac8b363
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-12 15:22:22 +0100

    pokeman starts

--- 0x01- World of DOM/Pokemon/app.js
+++ 0x01- World of DOM/Pokemon/app.js
@@ -1,19 +1,22 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+/**
+ * HIS OWN
+ */
+// const container = document.querySelector('#container');
+// const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+// for (let i = 1; i <= 151; i++) {
+//     const pokemon = document.createElement('div');
+//     pokemon.classList.add('pokemon');
+//     const label = document.createElement('span');
+//     label.innerText = `#${i}`;
+//     const newImg = document.createElement('img');
+//     newImg.src = `${baseURL}${i}.png`
 
 
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
-}
+//     pokemon.appendChild(newImg);
+//     pokemon.appendChild(label);
+//     container.appendChild(pokemon)
+// }
