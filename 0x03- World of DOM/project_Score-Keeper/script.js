84907355a4b76d3c402f779358408e11d26c7e7e
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 18:53:33 +0100

    Ping pong completed

--- 0x03- World of DOM/project_Score-Keeper/script.js
+++ 0x03- World of DOM/project_Score-Keeper/script.js
@@ -1,8 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const resetButton = document.querySelector('#resetGame');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -14,16 +15,39 @@ p1Button.addEventListener('click', function () {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
     }
-})
+});
+
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
         p2Display.textContent = p2Score;
     }
+});
+
+resetButton.addEventListener('click', reset)
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
 })
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('winner', 'loser');
+}
+
