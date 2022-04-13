29373ce6d27847cad25f63e72273853ab92157c2
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 15:47:27 +0100

    Updated scorekeeper buttons

---
+++ 0x03- World of DOM/project_Score-Keeper/script.js
@@ -0,0 +1,6 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+
+p1Button.addEventListener('click', function () {
+    alert('Clicked!!');
+})
