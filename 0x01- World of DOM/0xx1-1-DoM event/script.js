aba74814144f3a52c04dde8ec417163190041988
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 01:00:45 +0100

    end of add event listener

--- 0x01- World of DOM/0xx1-1-DoM event/script.js
+++ 0x01- World of DOM/0xx1-1-DoM event/script.js
@@ -16,6 +16,9 @@ btn2.onmouseleave = mousLeave
  * ADD Event Listener
  * ==================
  * Specify the evemt type and a callback to run
+ * - more control
+ * - more flexibility
+ * - more power.
  */
 const h2 = document.querySelector('h2');
 h2.addEventListener('click', () => alert('Users clicked on Header 2'));
@@ -27,5 +30,6 @@ function shout() {
     console.log('shout');
 }
 
-btn3.addEventListener('click', twist)// to have as many call backs as we want
+btn3.addEventListener('click', twist, { once: true })// to have as many call backs as we want
+//once to true makes the event runs only once on windows load
 btn3.addEventListener('click', shout)// to have as many call backs as we want
