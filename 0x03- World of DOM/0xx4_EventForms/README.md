382b1bfae986f9a856850f8ed4260a782596b849
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 08:48:54 +0100

    Form event completed

--- 0x03- World of DOM/0xx4_EventForms/README.md
+++ 0x03- World of DOM/0xx4_EventForms/README.md
@@ -4,3 +4,6 @@
 > instead I want to do something on my page with the data when submitted with javascript
 
 e.g commenting the page
+e.g Making a tweet chat app
+
+## Change and Input
--- 0x03- World of DOM/0xx4_EventForms/script.js
+++ 0x03- World of DOM/0xx4_EventForms/script.js
@@ -1,9 +1,26 @@
 const tweetForm = document.querySelector('#tweetForm');
 const textField = document.querySelector('#textField');
 
+const addTweet = function (username, userTweet) {
+    const newTweet = document.createElement('li');
+    const boldTag = document.createElement('b');
+    boldTag.append(username);
+    newTweet.append(boldTag);
+    newTweet.append(` - ${userTweet}`);
+    // console.log(newTweet);
+    textField.append(newTweet);
+}; //Add tweet Function Skeleton
+
 tweetForm.addEventListener('submit', function (e) {
-    const userinput = document.querySelectorAll('input')[0].value;
-    const userTweet = document.querySelectorAll('input')[1].value;
-    console.log(`${userinput} \n${userTweet}`)
-    e.preventDefault();
+    e.preventDefault(); //prevents get request
+
+    // const username = document.querySelectorAll('input')[0].value; 
+    // const userTweet = document.querySelectorAll('input')[1].value;
+
+    const usernameInput = tweetForm.elements.username; //Better alternative by name
+    const userTweet = tweetForm.elements.tweet; //Better alternative by name
+    addTweet(usernameInput.value, userTweet.value)
+    usernameInput.value = ''; //reset
+    userTweet.value = ''; //reset
 });
+
