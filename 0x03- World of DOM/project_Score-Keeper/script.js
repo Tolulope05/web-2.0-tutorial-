b8e0868a982f91403b1202e32b82f6c5b3dbefa6
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-13 16:16:08 +0100

    Game

--- 0x03- World of DOM/project_Score-Keeper/script.js
+++ 0x03- World of DOM/project_Score-Keeper/script.js
@@ -1,6 +1,29 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#resetGame');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
 
+let p1Score = 0;
+let p2Score = 0;
+let isGameOver = false;
+
+let winningScore = 5;
 p1Button.addEventListener('click', function () {
-    alert('Clicked!!');
+    if (!isGameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+})
+p2Button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 })
