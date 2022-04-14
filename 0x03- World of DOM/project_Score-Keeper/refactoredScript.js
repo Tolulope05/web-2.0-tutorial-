1c653d4c21cc587a5dead29d6be2f19c00a27947
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-14 07:04:50 +0100

    refactoredscript

---
+++ 0x03- World of DOM/project_Score-Keeper/refactoredScript.js
@@ -0,0 +1,58 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let isGameOver = false;
+let winningScore = 3;
+
+function updateScore(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('winner');
+            opponent.classList.add('loser');
+            player.disabled = true;
+            opponent.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+
+
+
+p1.button.addEventListener('click', function () {
+    updateScore(p1, p2)
+});
+
+p2.button.addEventListener('click', function () {
+    updateScore(p2, p1)
+});
+
+resetButton.addEventListener('click', reset)
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('winner', 'loser');
+        p.button.disabled = false;
+    }
+}
