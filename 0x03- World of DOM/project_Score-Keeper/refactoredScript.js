180017e1a99a1cd9839cbca614c6ea8fa821f8c0
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-14 08:50:43 +0100

    Not complete

--- 0x03- World of DOM/project_Score-Keeper/refactoredScript.js
+++ 0x03- World of DOM/project_Score-Keeper/refactoredScript.js
@@ -13,7 +13,7 @@ const p2 = {
 const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playto');
 let isGameOver = false;
-let winningScore = 3;
+let winningScore = 4;
 
 function updateScore(player, opponent) {
     if (!isGameOver) {
