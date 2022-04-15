8d8dd0578b0fc6c53fa2cefcda23fdf475cf2611
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-15 08:35:24 +0100

    checking fix

--- 0x04- Async Javascript/0xx2_PromisesIntro/creatPromise.js
+++ 0x04- Async Javascript/0xx2_PromisesIntro/creatPromise.js
@@ -14,6 +14,11 @@ fakeRequest('/tolu/tech/1')
         console.log('Done with Request')
         // console.log(data)
     })
+    .then((data) => {
+        console.log('It Worked (Page2)')
+        console.log(data)
+        return fakeRequestPromise('tolu/tech/page2')
+    })
     .catch(() => {
         console.log('hi juli')
     })
