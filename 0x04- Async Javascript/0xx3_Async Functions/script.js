a04260f5350c21532f8a5e2bd522c0ceca4766e6
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-15 20:47:09 +0100

    decalring an async

--- 0x04- Async Javascript/0xx3_Async Functions/script.js
+++ 0x04- Async Javascript/0xx3_Async Functions/script.js
@@ -8,6 +8,23 @@
  * - If the function throws an execption, the promise wil be rejected.
  */
 
-async function hello() {
+// async function hello() {}
+// } // Automatically returns New Promises
 
-}
+const sing = async () => {
+    // throw new Error('Async down')
+    throw ('402 Bad Request')
+    return 'LA LA LA'
+    /**
+     * If you throw an eror in aync, The promise is rejected
+     * If we throw a valur, Promise is accepted. 
+     */
+} //fulfiled 'la la la'
+
+sing()
+    .then((data) => console.log('Promise resolved eith:', data))
+    .catch((err) => console.log('Oh no, Promise rejected:', err))
+
+/** REjecting a promise
+ * - By throwing an error into the async function.
+ */
