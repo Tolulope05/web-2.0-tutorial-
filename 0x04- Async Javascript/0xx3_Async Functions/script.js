56b2c0cccbe0d4c5360c588a4f91e5396865f5f5
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-15 12:40:27 +0100

    Async always return promise

---
+++ 0x04- Async Javascript/0xx3_Async Functions/script.js
@@ -0,0 +1,13 @@
+/**
+ * ASync 
+ * =====
+ * keyword: async
+ * Keyword: await
+ * Async function always return a promise
+ * - If the function returns a value, the promise will be rsolved with that value.
+ * - If the function throws an execption, the promise wil be rejected.
+ */
+
+async function hello() {
+
+}
