b01783faf4c9c4bf750ebd6442310ca213ac443a
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-15 20:57:42 +0100

    username

--- 0x04- Async Javascript/0xx3_Async Functions/script.js
+++ 0x04- Async Javascript/0xx3_Async Functions/script.js
@@ -11,20 +11,35 @@
 // async function hello() {}
 // } // Automatically returns New Promises
 
-const sing = async () => {
-    // throw new Error('Async down')
-    throw ('402 Bad Request')
-    return 'LA LA LA'
-    /**
-     * If you throw an eror in aync, The promise is rejected
-     * If we throw a valur, Promise is accepted. 
-     */
-} //fulfiled 'la la la'
-
-sing()
-    .then((data) => console.log('Promise resolved eith:', data))
-    .catch((err) => console.log('Oh no, Promise rejected:', err))
-
 /** REjecting a promise
  * - By throwing an error into the async function.
  */
+
+// const sing = async () => {
+//     // throw new Error('Async down')
+//     throw ('402 Bad Request')
+//     return 'LA LA LA'
+//     /**
+//      * If you throw an eror in aync, The promise is rejected
+//      * If we throw a valur, Promise is accepted. 
+//      */
+// } //fulfiled 'la la la'
+
+// sing()
+//     .then((data) => console.log('Promise resolved eith:', data))
+//     .catch((err) => console.log('Oh no, Promise rejected:', err))
+
+const login = async function (username, password) {
+    if (!username || !password) throw 'Missing Credentials'
+    if (password === 'tolulope') return 'WELCOME!'
+    throw 'Invalid Password!'
+}
+
+login('username', 'tolul')
+    .then(msg => {
+        console.log('Logged in!')
+        console.log(msg)
+    })
+    .catch(err => {
+        console.log('Invalid Username & Password: ', err)
+    })
