047e1b2fda1b0c8a93352d1da15f948e82450ae7
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-15 11:35:05 +0100

    Promise success

--- 0x04- Async Javascript/0xx2_PromisesIntro/creatPromise.js
+++ 0x04- Async Javascript/0xx2_PromisesIntro/creatPromise.js
@@ -1,24 +1,62 @@
 
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const rand = Math.floor(Math.random() * 10) //0-9
+//         setTimeout(() => {
+//             if (rand <= 6) {
+//                 resolve('Your Fake Data Here')
+//             } else {
+//                 reject('Request Error!!')
+//             }
+//         }, 1500)
+//     })
+// }
 
-const fakeRequest = (url) => {
+// fakeRequest('/tolu/tech/1')
+//     .then((data) => {
+//         console.log('Done with REQUEST:')
+//         console.log('Data is:', data)
+//     })
+//     .catch((err) => {
+//         console.log('Oh NO!!', err)
+//     })
+
+
+// A Change Background Function 
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay * 1000)
+// }
+// delayedColorChange('green', 3, () => {
+//     delayedColorChange('teal', 3, () => {
+//         delayedColorChange('blue', 3, () => {
+//             delayedColorChange('orange', 3, () => {
+//                 delayedColorChange('purple', 3)
+//             })
+//         })
+//     })
+// }) // CHANGES BACKGROUND COLOR.
+
+// Using Promise to run the change background function
+
+const delayedColorChange = (newColor, delay) => {
     return new Promise((resolve, reject) => {
-        const rand = Math.floor(Math.random())
         setTimeout(() => {
-            resolve()
-        }, 1500)
+            document.body.style.backgroundColor = newColor;
+            resolve();
+        }, delay)
     })
 }
 
-fakeRequest('/tolu/tech/1')
-    .then((data) => {
-        console.log('Done with Request')
-        // console.log(data)
-    })
-    .then((data) => {
-        console.log('It Worked (Page2)')
-        console.log(data)
-        return fakeRequestPromise('tolu/tech/page2')
-    })
-    .catch(() => {
-        console.log('hi juli')
-    })
+delayedColorChange('red', 2000)
+    .then(() => delayedColorChange('red', 3000))
+    .then(() => delayedColorChange('green', 3000))
+    .then(() => delayedColorChange('blue', 3000))
+    .then(() => delayedColorChange('orange', 3000))
+    .then(() => delayedColorChange('yellow', 3000))
+    .then(() => delayedColorChange('indigo', 3000))
+    .then(() => delayedColorChange('violet', 3000))
+
