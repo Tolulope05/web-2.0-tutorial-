d6e5ec880c78faf5ea0c46ad17293f01acf525d9
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-18 04:24:55 +0100

    Success with tvshow app

--- 0x04- Async Javascript/project_TVSHOW-Search-App/script.js
+++ 0x04- Async Javascript/project_TVSHOW-Search-App/script.js
@@ -2,6 +2,25 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const searchTerm = form.elements.query.value
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    console.log(res.data[0].show.image.medium) // Image generated
+    const config = { params: { q: searchTerm } } // Other API Params can be addd via this on axios e.g {page: pageNumber}
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
+    console.log(res.data) // Image generated
+    // console.log(res.data[0].show.image) // Image generated
+    makeImages(res.data)
+    form.elements.query.value = '';
 })
+
+// A function that make new images on key press
+const makeImages = function (shows) {
+    for (result of shows) {
+        if (result.show.image) {
+            console.log(result)
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+
+    }
+}
+
+// Wrtie a function that clearn images on new key press
