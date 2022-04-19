5c0962174322af5ee7451452197127b434d20ab1
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-19 13:55:45 +0100

    extends

--- 0x04- Async Javascript/0xx4_Object-Classes-OOP/script2.js
+++ 0x04- Async Javascript/0xx4_Object-Classes-OOP/script2.js
@@ -6,12 +6,29 @@
  * - sharing functionality between classes
  */
 
-class Cat {
-    constructor(name, age) {
+
+class Pet {
+    constructor() {
         this.name = name;
         this.age = age;
     }
     eat() {
-        return `${this.name} is eating`
+        return `${this.name} is eating!!!`
     }
 }
+class Cat extends Pet {
+    meow() {
+        return `${this.name} is making Meeeooowwwwww Sound!!!`
+    }
+}
+
+
+const monthy = new Cat('monty', 9);
+
+class Dog extends Pet {
+    bark() {
+        return `${this.name} is making WoooFFF Wooof!!!`
+    }
+}
+
+const wyatt = new Dog('wyatt', 13)
