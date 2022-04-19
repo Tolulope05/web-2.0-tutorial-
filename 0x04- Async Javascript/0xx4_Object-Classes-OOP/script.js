0292ac03c74e2dc3e85b8671fa7b31dcea1f779c
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-19 08:34:56 +0100

    New object

--- 0x04- Async Javascript/0xx4_Object-Classes-OOP/script.js
+++ 0x04- Async Javascript/0xx4_Object-Classes-OOP/script.js
@@ -67,6 +67,19 @@ function Color(r, g, b) {
     this.r = r
     this.g = g
     this.b = b
+
+} // Constructor function for color
+
+Color.prototype.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`
+};
+
+Color.prototype.hex = function () {
+    const { r, g, b } = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1); //Converts Rgb to hexacedimal values
+
 }
 
-
+const color1 = new Color(40, 50, 60);
+const color2 = new Color(55, 67, 98);
