cebd39867a96910fe8423c044bc70fcb3da7f6d8
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-19 14:48:09 +0100

    Done with constructor

--- 0x04- Async Javascript/0xx4_Object-Classes-OOP/script2.js
+++ 0x04- Async Javascript/0xx4_Object-Classes-OOP/script2.js
@@ -1,12 +1,11 @@
 /**
- ***********************
+ *****************
  * EXTEND & SUPER
- ***********************
+ *****************
+ * EXTENDS
  * - subclassing, essentially inheritance
  * - sharing functionality between classes
  */
-
-
 class Pet {
     constructor(name, age) {
         this.name = name;
@@ -15,20 +14,37 @@ class Pet {
     eat() {
         return `${this.name} is eating!!!`
     }
-}
+} // Base Class 
 class Cat extends Pet {
+    constructor(name, age, livesLeft = 9) {
+        /**
+         * Using super keyword is going to refrence
+         * this.name = name;
+         * this.age = age;
+         * - Its going to call the constructor in pet
+         */
+        super(name, age)
+        this.livesLeft = livesLeft;
+    }
     meow() {
         return `${this.name} is making Meeeooowwwwww Sound!!!`
     }
-}
-
-
-const monthy = new Cat('monty', 9);
+} // dog Class that extend from Pet
 
 class Dog extends Pet {
     bark() {
         return `${this.name} is making WoooFFF Wooof!!!`
     }
-}
+    /**
+     * If we call another method that has been called in the  
+     * extended class, It defines that which is this class 
+     * before finding that which is in the extended class.
+     */
+    eat() {
+        return `${this.name} scarfs his food`;
+    }
+} // dog Class that extend from Pet
 
+
+const monthy = new Cat('monty', 9);
 const wyatt = new Dog('wyatt', 13)
