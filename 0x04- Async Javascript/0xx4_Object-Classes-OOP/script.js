696028359b2e304179f126e46d1d04e809859bc2
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-19 07:46:20 +0100

    constructor Function

--- 0x04- Async Javascript/0xx4_Object-Classes-OOP/script.js
+++ 0x04- Async Javascript/0xx4_Object-Classes-OOP/script.js
@@ -17,27 +17,32 @@
  * OBJECT ORIENTED CONCEPTS IN Js
 */
 
-// Factory Function
-
-function hex(r, g, b) {
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1); //Converts Rgb to hexacedimal values
-} // console.log(hex(255, 255, 255));
-
-function rgb(r, g, b) {
-    return `rgb(${r}, ${g}, ${b})`
-}
-
-"hex(55,100,25)"
-'rgb(56, 34, 34)'
-
+// 1. Factory Function
+// function rgb(r, g, b) {
+//     return `rgb(${r}, ${g}, ${b})`
+// }
+// 'rgb(56, 34, 34)'
 function makeColor(r, g, b) {
-    const color = {}
-    color.r = r;
+    const color = {} //Makes an object
+    color.r = r; //gives it property based on the arguments that we are provided
     color.g = g;
-    color.b = b
-    color.rgb = dunction() {
+    color.b = b;
+    color.rgb = function () {
+        const { r, g, b } = this; // Destructure rgb from this i.e color object
         return `rgb(${r}, ${g}, ${b})`
-    }
+    };
+    color.hex = function hex() {
+        const { r, g, b } = this; // Destructure rgb from this i.e color object
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1); //Converts Rgb to hexacedimal values
+    } // console.log(hex(255, 255, 255))
     return color;
 }
+const firstColor = makeColor(35, 255, 150);
+firstColor.rgb(); //'rgb(35, 255, 150)'
+firstColor.hex(); //'#23ff96'
 
+const black = makeColor(0, 0, 0)
+black.rgb();
+
+
+// 2. Constructor Function {Better Alternative}
