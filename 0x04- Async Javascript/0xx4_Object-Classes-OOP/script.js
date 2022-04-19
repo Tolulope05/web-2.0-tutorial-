3bd73b3fb5ff777e32c6f215641adf799dbeec9c
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-19 12:45:41 +0100

    Calc hsl implemented in the color constructor

--- 0x04- Async Javascript/0xx4_Object-Classes-OOP/script.js
+++ 0x04- Async Javascript/0xx4_Object-Classes-OOP/script.js
@@ -138,6 +138,50 @@ class Color {
         return `rgba(${this.innerRGB()},${a})`;
     }
 
+    calcHSL() {
+        const { r, g, b } = this;
+        return `${r},${g},${b}`;
+
+        // Make r,g,b fraction of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        // Find greatest and smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            // Red is max
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            // Green is max
+            h = ((b - r) / delta) + 2;
+        else
+            // Blue is max
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        // Make negative hues positive behind 360 
+        if (h < 0) h += 360;
+        // Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        // Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        // Multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+
+        // Return value of h, s and l as an array
+        return [h, s, l]
+    }
 }
 
 const red = new Color(255, 67, 89, 'tomato');
