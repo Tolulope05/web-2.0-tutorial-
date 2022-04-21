cec0339b16877ffe7c7af6f562e3fb0838146ea0
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-21 21:51:19 +0100

    Require implemented

---
+++ 0x06- Node Js/0xx2_Modules_NPM/01_math.js
@@ -0,0 +1,10 @@
+/**
+ * We will use the module.exports object to export our functions
+ */
+const add = (x, y) => x + y;
+
+const pi = 3.14;
+
+const square = (x) => x * x;
+
+module.exports = { add, pi, square };
---
+++ 0x06- Node Js/0xx2_Modules_NPM/02_app.js
@@ -0,0 +1,10 @@
+// const math = require('./01_math');
+// console.log(math.square(8));
+
+const { pi, square, add } = require('./01_math');
+
+
+console.log(square(8));
+console.log(add(2, 3));
+console.log(pi);
+
