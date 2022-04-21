7ba53520be54f2f8f160d7e31ec0936722fa75ac
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-21 21:58:35 +0100

    Module

--- 0x06- Node Js/0xx2_Modules_NPM/01_math.js
+++ 0x06- Node Js/0xx2_Modules_NPM/01_math.js
@@ -7,4 +7,16 @@ const pi = 3.14;
 
 const square = (x) => x * x;
 
-module.exports = { add, pi, square };
+// module.exports.add = add;
+// module.exports.pi = pi;
+// module.exports.square = square;
+
+// module.exports = { add, pi, square };
+
+const math = {
+    add: add,
+    pi: pi,
+    square: square
+}
+
+module.exports = math;
