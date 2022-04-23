ba00e3f2eafb95e08a6f44ac8c348664b997fc15
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-23 23:18:29 +0100

    Ejs sucess

--- 0x07_Express/0xx2_EJS-Templating/Templating_Demo/index.js
+++ 0x07_Express/0xx2_EJS-Templating/Templating_Demo/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path') // A module built into Express
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 });
 
-// => Telling the App to use EJS
-app.set('view engine', 'ejs');
+// => Telling the App to use EJS files
+app.set('view engine', 'ejs'); // This wont work if we call nodedemon in another directory
+
+// To give us ability to call index.js from another directory apart from the home directory
+app.set('views', path.join(__dirname, '/views')) // to be able to run index.js outside templatinf demo 
+
 
 // => Root Route
 app.get('/', (req, res) => {
     console.log('New get request on /')
-    res.send('<h1>Hello Bro, How are you today</h1>')
+    res.render('home')
 });
 
