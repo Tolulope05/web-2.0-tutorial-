a0b4600b981cd08262bdd3d306e32b86f5acb00d
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-23 03:19:06 +0100

    Success

--- 0x06- Node Js/0xx6_langGuesser/index.js
+++ 0x06- Node Js/0xx6_langGuesser/index.js
@@ -1,5 +1,6 @@
 import { franc, francAll } from 'franc'
 import langs from 'langs'
+import colors from 'colors'
 const input = process.argv[2]
 
 const langCode = franc(input)
@@ -19,9 +20,9 @@ const language = langs.where("3", langCode)
 // } // Method 2
 
 if (langCode === 'und') {
-    console.log("Sorrry, Couldn'tOULDN'T FIGURE IT OUT, TRY WITH MORE SAMPLE TEXT!")
+    console.log("Sorrry, Couldn'COULDN'T FIGURE IT OUT, TRY WITH MORE SAMPLE TEXT!".red)
 } else {
-    console.log(`OUR BEST GUESS IS: ${language.name}`);
+    console.log(`OUR BEST GUESS IS: ${language.name}`.green);
 } // Method 3
 
 
