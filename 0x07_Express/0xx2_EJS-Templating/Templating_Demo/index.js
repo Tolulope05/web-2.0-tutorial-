81ea5ad7e5984309f4f974a9d4ebcafef481c6fd
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-23 21:29:29 +0100

    Ejs input

--- 0x07_Express/0xx2_EJS-Templating/Templating_Demo/index.js
+++ 0x07_Express/0xx2_EJS-Templating/Templating_Demo/index.js
@@ -1,7 +1,17 @@
-const express = require('events');
+const express = require('express');
 const app = express();
 
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 });
+
+// => Telling the App to use EJS
+app.set('view engine', 'ejs');
+
+// => Root Route
+app.get('/', (req, res) => {
+    console.log('New get request on /')
+    res.send('<h1>Hello Bro, How are you today</h1>')
+});
+
