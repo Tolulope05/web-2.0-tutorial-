208b668590a3f8e921c892a53dbe8f4db105d6f6
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-26 22:01:25 +0100

    get tarcos response

--- 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/index.js
+++ 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/index.js
@@ -3,8 +3,11 @@ const app = express();
 
 
 const port = 3000;
-
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
+
+app.get("/tarcos", (req, res) => {
+    res.send("GET /tarcos response");
+})
 
