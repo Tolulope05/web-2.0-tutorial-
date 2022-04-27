074c909ee7a55c9f78cbe599990f7a6d540624c4
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-27 15:47:37 +0100

    resstful in progress

---
+++ 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/README.md
@@ -0,0 +1,12 @@
+## Restfulness with Express
+
+| NAME       |  PATH      |   VERB    | PURPOSE  |
+|------------|------------|------------|---------|
+| `GET`      | `/api/todos`| `GET`     | `Get all todos` |
+| `GET`      | `/api/todos/:id`| `GET`     | `Get a todo` |
+| `POST`     | `/api/todos`| `POST`    | `Create a todo` |
+| `PUT`      | `/api/todos/:id`| `PUT`    | `Update a todo` |
+| `DELETE`   | `/api/todos/:id`| `DELETE`  | `Delete a todo` |
+| `DELETE`   | `/api/todos/:id`| `DELETE`  | `Delete a todo` |
+| `DELETE`   | `/api/todos/:id`| `DELETE`  | `Delete a todo` |
+
--- 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/index.js
+++ 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/index.js
@@ -1,26 +1,34 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 
 /**
- * CRUD FUNCTIONALITY WITH REST
+ * CRUD FUNCTIONALITY WITH RESTFUL APIS
  * ============================ 
+ *  >> HTTP VERBS 
  *  GET /comments - list all omments
  *  POST /comments - Create a new comment
  *  GET /comments/:id - Get one comment (Using ID)
- *  PATCH /comments
+ *  PATCH /comments/:id - Update One Comment
+ *  DELETE /comments/:id - Removw one comment
  */
 
+app.use(express.urlencoded({ extended: true })) // Turns POST req.body from undefined to { meat: "'Chicken Beef'", qty: '5' }
+app.use(express.json())
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
 
-app.get("/tarcos", (req, res) => {
+app.get("/tacos", (req, res) => {
     res.send("GET /tarcos response");
 })
-app.post("/tarcos", (req, res) => {
+app.post("/tacos", (req, res) => {
     res.send("GET /tarcos post response");
+    console.log(req.body)
 })
 
 app.set
