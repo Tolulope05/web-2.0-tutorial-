8ba112ad32b541819417091032376e996311669d
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-27 15:13:53 +0100

    Crud functionality with restfulness

--- 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/index.js
+++ 0x07_Express/0xx3_Defining-Express-Post-Routes/RESTDemo/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+/**
+ * CRUD FUNCTIONALITY WITH REST
+ * ============================ 
+ *  GET /comments - list all omments
+ *  POST /comments - Create a new comment
+ *  GET /comments/:id - Get one comment (Using ID)
+ *  PATCH /comments
+ */
+
 
 const port = 3000;
 app.listen(port, () => {
