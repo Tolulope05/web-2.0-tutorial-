f1f3495cac8cd227b9503a43591162a76b6658bd
Tolulope05 <fakunletolulope05@gmail.com>
2022-04-29 14:13:56 +0100

    Extra mongo function data success and tested

---
+++ 0x08-MongoDB/script.js
@@ -0,0 +1,14 @@
+const dogs = [
+    {
+        name: "Bruno", breed: "Labrador", age: 3, weight: 4, size: "medium", personality: { catFriendly: true, dogFriendly: true, kidFriendly: false, petFriendly: true }
+    },
+    {
+        name: "Buddy", breed: "Chihuahua", age: 5, weight: 3, size: "small", personality: { catFriendly: true, dogFriendly: false, kidFriendly: false, petFriendly: false }
+    },
+    {
+        name: "Bella", breed: "Corgi", age: 10, weight: 8, size: "medium", personality: { catFriendly: false, dogFriendly: false, kidFriendly: true, petFriendly: true }
+    },
+    {
+        name: "Dodger", breed: "Corgi", age: 12, weight: 15, size: "large", personality: { catFriendly: false, dogFriendly: true, kidFriendly: true, petFriendly: false }
+    }
+] //Object used for Mono Db Operational function
