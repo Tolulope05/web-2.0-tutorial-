efbbc37cce99aa81fc663adaa0b3df9a524dd458
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-01 12:23:16 +0100

    model created and data inputed

--- 0x09-Mongoose_Express/seeds.js
+++ 0x09-Mongoose_Express/seeds.js
@@ -19,3 +19,40 @@ mongoose.connect('mongodb://localhost:27017/farmStand') // Creates a database ca
 //     .then(d => console.log(d))
 //     .catch(e => console.log(e))
 
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'diary'
+
+    }
+]
+
+
+Product.insertMany(seedProducts)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
