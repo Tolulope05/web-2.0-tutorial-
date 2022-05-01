ecc3c7d4656e462d1a2fe33f867cdc6e156bb06d
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-01 17:11:36 +0100

    post request setup

--- 0x09-Mongoose_Express/index.js
+++ 0x09-Mongoose_Express/index.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand') // Creates a database ca
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/product', async (req, res) => {
     const products = await Product.find({})
@@ -23,6 +24,15 @@ app.get('/product', async (req, res) => {
     console.log(products)
     console.log('New request on product/index');
 });
+
+/**CREATING NEW PRODUCT */
+app.get('/product/new', (req, res) => {
+    res.render('products/new')
+})
+app.post('products', (req, res) => {
+    console.log(req.body);
+    res.send('making your products');
+}) // reason we use app.use urlencoded
 
 app.get('/product/:id', async (req, res) => {
     const { id } = req.params;
