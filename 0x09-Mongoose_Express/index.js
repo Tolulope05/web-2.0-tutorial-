96c1d9a94362b562b19d5f4cf4c238aeea3240ff
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-02 10:05:15 +0100

    Success with refactoring the app

--- 0x09-Mongoose_Express/index.js
+++ 0x09-Mongoose_Express/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+var methodOverride = require('method-override')
 
 const Product = require('./models/product')
 
@@ -17,32 +18,50 @@ mongoose.connect('mongodb://localhost:27017/farmStand') // Creates a database ca
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method')); // override with POST having ?_method=PUT
 
 app.get('/product', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products });
-    console.log(products)
+    // console.log(products)
     console.log('New request on product/index');
 });
 
 /**CREATING NEW PRODUCT */
 app.get('/product/new', (req, res) => {
     res.render('products/new');
-    console.log('New  request on Product page')
+    console.log('New request on Product page')
 })
 app.post('/products', async (req, res) => {
     // console.log(req.body);
     const newProduct = new Product(req.body); // Saves the new product to the database
-    await newProduct.save()
+    await newProduct.save();
     console.log(newProduct);
+    res.redirect(`/products/${newProduct._id}`)
 }) // reason we use app.use urlencoded
 
-app.get('/product/:id', async (req, res) => {
+app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    console.log('new request on port/:id');
     res.render('products/show', { product });
+    console.log(`new request to display ${product.name} `);
 });
+
+/**UPDATING A PRODUCT */
+// This will require a package called method override so as to chamge a post request to a put request
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id);
+    res.render('products/edit', { product });
+    console.log(`new request to edit ${product.name} `)
+})
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true }); //Arguments: id | how we want to update | options
+    // console.log(req.body);
+    res.redirect(`/products/${product._id}`)
+
+})
 
 const port = 3000;
 app.listen(port, () => {
