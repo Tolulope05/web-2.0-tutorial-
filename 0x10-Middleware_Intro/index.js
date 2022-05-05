dc9890dac49b529c3c9e78302b3217e64e70c904
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-05 15:09:13 +0100

    wrong file cleaned

--- 0x10-Middleware_Intro/index.js
+++ 0x10-Middleware_Intro/index.js
@@ -5,7 +5,9 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
-    req.requestTime = Date.now();
+    // console.log(req.method, req.path);
+    // req.method = 'GET'; makes all method sent a GET Request.
+    req.requestTime = Date.now(); // This was used in all other requests out there.
     console.log(req.path, req.method)
     next();
 });
@@ -15,14 +17,48 @@ app.use('/dogs', (req, res, next) => {
     next();
 })
 
+/**PASSWORD MIDDLEWARE DEMO 
+ * - Using a Middleware to protect a Route.
+ * - This is a Fake Authentication
+*/
+// app.use('/secret', (req, res, next) => {
+//     // res.send('Query Seen')
+//     const { password } = req.query;
+//     if (password === 'tolulope') {
+//         next();
+//     } else {
+//         res.send('Sorry You Need a Correct Password to access this link, \n Append ?password=YOURPASSWORD into the query string')
+//     }
+//     /** FOR EVERY ROUTE WE NEED TO PASS A QUERY STRING AND PASSWORD FOR IT TO WORK */
+// })
+/* WE CAN ALSO USE */
 const verifyPassword = (req, res, next) => {
+    // res.send('Query Seen')
     const { password } = req.query;
     if (password === 'tolulope') {
         next();
     } else {
         res.send('Sorry You Need a Correct Password to access this link, \n Append ?password=YOURPASSWORD into the query string.')
     }
-}
+    /** FOR EVERY ROUTE WE NEED TO PASS A QUERY STRING AND PASSWORD FOR IT TO WORK */
+} // We want verify password to run first before any other request.
+
+
+// app.use((req, res, next) => {
+//     console.log('Hey Coder, This is my First MiddleWare!!');
+//     return next();
+//     console.log('Hey Coder, This is my First MiddleWare after calling Next()');
+// }); //defining middleware
+
+// app.use((req, res, next) => {
+//     console.log('Hey Coder, This is my Second Middleware!!');
+//     return next()
+// }); //defining middleware
+
+// app.use((req, res, next) => {
+//     console.log('Hey Coder, This is my Third Middleware!!');
+//     return next()
+// }); //defining middleware
 
 app.get('/', (req, res) => {
     console.log(`REQUEST DATE: ${req.requestTime}`);
