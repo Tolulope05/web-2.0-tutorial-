577b2a5edd8bb77a77099dbbc9154cbf8ecd2949
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-07 13:05:05 +0100

    defining farm and product models

---
+++ 0x12-DataRelationship_Mongo/0xx2_Mongo-Relationship_withExpress/README.md
@@ -0,0 +1,13 @@
+## Mongo Relationship extends Integrating Mongoose with Express
+
+INITIAL > 
+Full CRUD with Express and Mongoose that is connected to Html Css and Js files.
++
+Filering by Category
+
+Application to manage the inventory in a Farm Stead
+> How many things are sold out
+> How many things are left
+> How many things are in stock
+> How many things are in the warehouse
+
---
+++ 0x12-DataRelationship_Mongo/0xx2_Mongo-Relationship_withExpress/seeds.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product')
+
+mongoose.connect('mongodb://localhost:27017/farmStandTake2') // Creates a database called movieApp
+    .then(() => {
+        console.log('Mongo Connection OPEN')
+    })
+    .catch((err) => {
+        console.log('Mongo Connection Failed!!')
+        console.log(err)
+    })
+
+// const p = new Product({
+//     name: 'Ruby GrapeFruit',
+//     price: 1.99,
+//     category: 'fruit'
+// })
+// p.save()
+//     .then(d => console.log(d))
+//     .catch(e => console.log(e))
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+
+    }
+] // All items in this array needs to be validated before the insertMany will work
+
+
+Product.insertMany(seedProducts)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+// NB: We only use this script to seed the database, Its not part of the APP Operation
