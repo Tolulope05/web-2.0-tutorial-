a5bb3ab00605fcfa23e729750896d7d992f924b7
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-07 17:51:01 +0100

    Farm Show Page Success

--- 0x12-DataRelationship_Mongo/0xx2_Mongo-Relationship_withExpress/index.js
+++ 0x12-DataRelationship_Mongo/0xx2_Mongo-Relationship_withExpress/index.js
@@ -37,6 +37,11 @@ app.post('/farms', async (req, res) => {
     res.redirect('/farms')
 });
 
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id);
+    res.render('farms/show', { farm })
+})
+
 
 
 
