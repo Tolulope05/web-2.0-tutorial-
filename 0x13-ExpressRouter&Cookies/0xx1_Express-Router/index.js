c1bb9e4a25b61fba2dfeb78c71cad69b38caed6e
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-08 10:18:09 +0100

    route and middleware succss

--- 0x13-ExpressRouter&Cookies/0xx1_Express-Router/index.js
+++ 0x13-ExpressRouter&Cookies/0xx1_Express-Router/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const shelterRoutes = require('./routes/shelters');
 const dogRoutes = require('./routes/dogs');
+const adminRoutes = require('./routes/admin');
 
 /*EXPRESS ROUTER*/
 app.use('/shelters', shelterRoutes);
 app.use('/dogs', dogRoutes);
+app.use('/admin', adminRoutes);
 
 
 app.listen('3000', () => {
---
+++ 0x13-ExpressRouter&Cookies/0xx1_Express-Router/routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.use((req, res, next) => {
+    if (req.query.isAdmin) {
+        next();
+    }
+    res.send('SORRY, YOU ARE NOT AN ADMIN');
+}) // This midleware applies to only this route
+
+router.get('/', (req, res) => {
+    res.send('THIS IS THE ADMIN LEVEL, TURN BACK GEE')
+});
+
+router.get('/topsecrets', (req, res) => {
+    res.send('THIS IS TOP SECRET')
+});
+
+router.get('/deleteEverything', (req, res) => {
+    res.send('Ok!!..DELETED EVERYTHING')
+});
+
+module.exports = router;
