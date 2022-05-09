05f0116c3ae8e7095257aa332e6f0efbf03bdc8e
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-09 06:08:32 +0100

    Sending cookies

--- 0x13-ExpressRouter&Cookies/0xx2_CookiesDemo/index.js
+++ 0x13-ExpressRouter&Cookies/0xx2_CookiesDemo/index.js
@@ -5,10 +5,13 @@ app.get('/greet', (req, res) => {
     res.send('Hey there!');
 })
 
-app.get('/greet', (req, res) => {
-    res.send('Hey there!');
+/**SENDING COOKIES */
+app.get('/setname', (req, res) => {
+    res.cookie('name', 'Henrietta');
+    res.cookie('animal', 'Harlequin ');
+    res.send('Cookie set!');
 })
 
 app.listen('3000', () => {
     console.log('Server is running on port 3000');
-});
+})
