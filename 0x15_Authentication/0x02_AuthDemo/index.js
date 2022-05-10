34bf8dbfc2c70195cbd3603f639175a150011fce
Tolulope05 <fakunletolulope05@gmail.com>
2022-05-10 13:21:11 +0100

    logout session added

--- 0x15_Authentication/0x02_AuthDemo/index.js
+++ 0x15_Authentication/0x02_AuthDemo/index.js
@@ -4,6 +4,7 @@ const User = require('./models/user');
 const path = require('path');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/authDemo') // Creates a database called farmStand
     .then(() => {
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true })); // Parsing the request body
+app.use(session({ secret: 'notagoodsecret', resave: true, saveUninitialized: true }));
 
 app.get('/', (req, res) => {
     res.send('This is the Home Page!');
@@ -34,6 +36,7 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/')
 });
 
@@ -45,14 +48,24 @@ app.post('/login', async (req, res) => {
     const user = await User.findOne({});
     const validUser = await bcrypt.compare(password, user.password);
     if (validUser) {
-        res.send('YAH WELCOME')
+        req.session.user_id = user._id;
+        res.redirect('/secret')
     } else {
-        res.send('Try Again')
+        res.redirect('/login')
     }
 });
 
+app.post('/logout', async (req, res) => {
+    // req.session.user_id = null; // Mrthod 1
+    req.session.destroy();
+    res.redirect('/login')
+})
+
 app.get('/secret', (req, res) => {
-    res.send('THIS IS SECRET, YOU CANNOT SEE ME UNLESS YOU\'RE LOGGED IN');
+    if (!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    res.render('secret')
 });
 
 app.listen(3000, () => {
